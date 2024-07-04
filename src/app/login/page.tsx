e425c0199e842b7e4ce09f388de73aa9e32f4289
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-07-04 09:21:47 +0530

    complete

--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function login() {
-    return (
-        <div>
-            <h2>login</h2>
-        </div>
-    )
-}
--- src/app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ src/app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -1,11 +1,15 @@
+"use client"
+
 import Link from 'next/link'
 import styles from './menuLink.module.css'
 import { usePathname } from 'next/navigation'
 
-const MenuLink = ({ item }) => {
-  // const pathname = usePathname()
+const MenuLink = ({item}) => {
+
+  const pathname = usePathname()
+
   return (
-    <Link href={item.path} className={`${styles.container} `}>
+    <Link href={item.path} className={`${styles.container} ${pathname === item.path && styles.active}`}>
       {item.icon}
       {item.title}
     </Link>
